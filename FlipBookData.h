15ad327323976cd4ddcf7f536b685e80d7f870c8
Jae Hyun Yoo <fkkcloud@gmail.com>
2016-05-03 16:11:50 -0700

    fx pipeline codes

---
+++ FlipBookData.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "FlipBookData.generated.h"
+
+UCLASS()
+class SHOOTERGAME_API AFlipBookData : public AActor
+{
+	GENERATED_UCLASS_BODY()
+	
+public:	
+	/** Speed for the flipbook animation */
+	UPROPERTY(EditAnywhere, Category = "Flipbook Mesh")
+	EFlippBookSpeed FlipBookFPS;
+
+	UPROPERTY(EditAnywhere, Category = "Flipbook Mesh")
+	TArray< UStaticMesh* > MeshArray;
+
+	UPROPERTY(EditAnywhere, Category = "Flipbook Mesh")
+	FRotator RotationOffset;
+};
